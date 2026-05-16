a66205a31c6793019f622fb56bbc8ed0e7606a37
ogundiran kevin <temikevin3@gmail.com>
2026-05-16 07:47:57 +0100

    fix school logo path

--- obj/Debug/net8.0/StudentComplaintSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentComplaintSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentComplaintSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4a84f044a92e3938cf5a1e7effd3dbcca9b70d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b129288f2f2341efed29efd920cc7d3444713189")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentComplaintSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentComplaintSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
